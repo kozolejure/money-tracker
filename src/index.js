33ecbf0b7226c55bc2463ab57358c47e7e5034aa
Jure Kozole <Kozole.jure@gmail.com>
2024-12-03 09:43:37 +0100

    hotjar

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
+// Hotjar Tracking Code for https://money-tracker-8lcv.vercel.app/
+(function (h, o, t, j, a, r) {
+  h.hj =
+    h.hj ||
+    function () {
+      (h.hj.q = h.hj.q || []).push(arguments);
+    };
+  h._hjSettings = { hjid: 5227678, hjsv: 6 };
+  a = o.getElementsByTagName('head')[0];
+  r = o.createElement('script');
+  r.async = 1;
+  r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
+  a.appendChild(r);
+})(window, document, 'https://static.hotjar.com/c/hotjar-', '.js?sv=');
+
 import { createBrowserHistory } from 'history';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
@@ -13,8 +28,6 @@ Sentry.init({
   dsn: 'https://5ae855d4c1d840c1b06679123069574f@sentry.io/1335198'
 });
 
-const siteId = 5227678;
-const hotjarVersion = 6;
 const store = configureStore();
 const history = createBrowserHistory();
 const container = document.getElementById('root');
@@ -22,7 +35,6 @@ const root = createRoot(container);
 root.render(<Root store={store} history={history} />);
 
 registerServiceWorker();
-Hotjar.init(siteId, hotjarVersion);
 
 if (module.hot) {
   module.hot.accept();
